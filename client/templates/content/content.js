9c512d62f2e9d6224a1d0d729ea574157d2e0156
sunny-g <sunny.gonna@gmail.com>
2015-06-15 19:49:07 -0700

    blah

---
+++ client/templates/content/content.js
@@ -0,0 +1,18 @@
+Template.content.helpers({
+  contentElements: function() {
+    return [
+      {
+        title: 'card 1',
+        comment: 'this is comment 1'
+      },
+      {
+        title: 'card 2',
+        comment: 'here\'s another comment'
+      },
+      {
+        title: 'card 3',
+        comment: 'what\'s happening?!'
+      }
+    ]
+  }
+});
